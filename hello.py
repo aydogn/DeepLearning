f4c6c79dcccb23fe6bd1f3b06e36469ed92b632a
aydogn <aydognabdullah@gmail.com>
2024-09-13 19:05:15 +0300

    pytorch çalışma yaptım

--- hello.py
+++ hello.py
@@ -1,1 +1,25 @@
-print('hello github!')
+"""import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+num_epochs =4
+batch_size = 4
+learning_rate = 0.001
+
+
+transform = transform.Compose()
+"""
+import torch 
+x = torch.rand(2,3)
+y = x.view(-1,2)
+print(type(x.size()))
+print(type(x.shape))
+print(y)
