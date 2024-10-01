ac813d0b4eb7728a4e7808790292ee8290c75fa6
aydogn <aydognabdullah@gmail.com>
2024-10-02 00:52:05 +0300

    logistic regression

--- hello.py
+++ hello.py
@@ -26,3 +26,45 @@ y_test = torch.from_numpy(y_test.astype(np.float32))
 
 y_train = y_train.view(y_train.shape[0],1)
 y_test = y_test.view(y_test.shape[0],1)
+
+# 1) model
+class LogisticRegression(nn.Module):
+    def __init__(self, n_input_features):
+        super().__init__()
+        self.linear = nn.Linear(n_input_features, 1)
+
+    def forward(slef, x):
+        y_predicted = torch.sigmoid(slef.linear(x))
+        return y_predicted
+    
+model = LogisticRegression(n_feature)
+
+# 2) loss & optimizer
+
+l_r = 0.01
+criterion = nn.BCELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=l_r)
+
+# 3) training loop
+num_epochs = 300
+for epoch in range(1,num_epochs+1):
+    #forward
+    y_predicted = model(X_train)
+    loss = criterion(y_predicted, y_train)
+    
+    #bacward
+    loss.backward()
+    
+    #update
+    optimizer.step()
+    optimizer.zero_grad()
+
+    if epoch % 10 == 0:
+        print(f'epoch: {epoch}, loss: {loss.item():.4f}')
+    
+with torch.no_grad():
+    y_predicted = model(X_test)
+    y_predicted_cls = y_predicted.round()
+    acc = y_predicted_cls.eq(y_test).sum() / float(y_test.shape[0])
+    print(f'accuracy = {acc:.4f}')
+
