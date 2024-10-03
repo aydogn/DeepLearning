2805e219c2a7a062bd7901c852f2e88b0e916bd6
aydogn <aydognabdullah@gmail.com>
2024-10-03 12:24:58 +0300

    transformlar üzerine çalıştım

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+""" 
 import torch 
 import torch.nn as nn
 import numpy as np
@@ -68,3 +69,86 @@ with torch.no_grad():
     acc = y_predicted_cls.eq(y_test).sum() / float(y_test.shape[0])
     print(f'accuracy = {acc:.4f}')
 
+"""
+
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import math
+
+class WineDataset(Dataset):
+    def __init__(self, transform=None) -> None:
+        xy = np.loadtxt('C:/Users/aydne/OneDrive/Desktop/DeepLearning/wine.csv', delimiter=',', dtype=np.float32, skiprows=1)
+        self.x = xy[:,1:]
+        self.y = xy[:,[0]]
+        self.n_samples = xy.shape[0]
+        self.transform = transform
+
+    def __getitem__(self, index):
+        sample = self.x[index], self.y[index]
+        if self.transform:
+            sample = self.transform(sample)
+        return sample 
+    
+    def __len__(self):
+        return self.n_samples
+
+class ToTensor:
+    def __call__(self, sample) -> None:
+        inputs, targets = sample
+        return torch.from_numpy(inputs), torch.from_numpy(targets)
+    
+class MulTransform:
+    def __init__(self, factor):
+        self.factor = factor
+    
+    def __call__(self, sample):
+        inputs, target = sample
+        inputs *= self.factor
+        return inputs, target
+list
+class dividedN:
+    def __init__(self, n) -> None:
+        self.n = n
+    def __call__(self, sample):
+        inputs, target = sample
+        inputs %= self.n
+        return inputs, target
+
+dataset = WineDataset(transform=ToTensor())
+first_data = dataset[0]
+features, labels = first_data
+print(features)
+print(type(features), type(labels))
+
+composed = torchvision.transforms.Compose([ToTensor(),MulTransform(2)])
+dataset = WineDataset(transform=composed)
+first_data = dataset[0]
+features, labels = first_data
+print(features)
+print(type(features), type(labels))
+
+composed = torchvision.transforms.Compose([ToTensor(),MulTransform(2), dividedN(10)])
+dataset = WineDataset(transform=composed)
+first_data = dataset[0]
+features, labels = first_data
+print(features)
+print(type(features), type(labels))
+"""
+dataloader = DataLoader(dataset=dataset, batch_size=4, shuffle=True, num_workers=0)
+
+num_epochs = 2
+total_samples = len(dataset)
+n_iterations = math.ceil(total_samples/4)
+print(total_samples, n_iterations)
+
+for epoch in range(num_epochs):
+    for i, (inputs, labels) in enumerate(dataloader):
+        #forward
+        #bacward
+        #update
+        if (i+1)%5==0:
+            print(f'epoch: {epoch+1}/{num_epochs}, step {i}/{n_iterations}, inputs: {inputs.shape}')
+
+"""
